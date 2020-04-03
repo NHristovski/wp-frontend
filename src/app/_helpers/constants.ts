0a17bbfe35e010b9ecff25c7ce807759788db2a5
Nikola Hristovski <nikolahristovski1122@gmail.com>
2020-04-03 23:05:45 +0200

    Heroku

--- src/app/_helpers/constants.ts
+++ src/app/_helpers/constants.ts
@@ -1,4 +1,4 @@
 export class Constants {
-  public static baseApiUrl = 'http://localhost:8762'; // 'http://api-gateway:8762'; // http://localhost:8762'; //
-  // public static baseApiUrl = 'https://wp-api-gateway.herokuapp.com';
+  // public static baseApiUrl = 'http://localhost:8762'; // 'http://api-gateway:8762'; // http://localhost:8762'; //
+  public static baseApiUrl = 'https://wp-api-gateway.herokuapp.com';
 }
